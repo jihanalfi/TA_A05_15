4a78c84c36cc4e84e85687344f2c091f23fe4d44
Jihan Alfi <jihanalfi67@gmail.com>
2021-12-09 23:11:03 +0700

    feat: produce API list cabang

---
+++ src/main/java/APAP/SIRETAILA0515/rest/Setting.java
@@ -0,0 +1,6 @@
+package APAP.SIRETAILA0515.rest;
+
+public class Setting {
+    final public static String item = "https://si-item.herokuapp.com";
+    final public static String itemRequest = "https://6486492c-2c38-4046-a782-d403388ab6e3.mock.pstmn.io/";
+}
