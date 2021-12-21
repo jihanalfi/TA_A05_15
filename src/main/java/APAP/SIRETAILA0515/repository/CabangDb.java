3a78b5a1a2e6249b48c0983a92373c351aed1182
Jihan Alfi <jihanalfi67@gmail.com>
2021-12-21 19:29:51 +0700

    feat: approval request cabang

--- src/main/java/APAP/SIRETAILA0515/repository/CabangDb.java
+++ src/main/java/APAP/SIRETAILA0515/repository/CabangDb.java
@@ -4,8 +4,10 @@ import APAP.SIRETAILA0515.model.CabangModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CabangDb extends JpaRepository<CabangModel, Long> {
+    List<CabangModel> findCabangModelsByStatusEquals(Long status);
 }
