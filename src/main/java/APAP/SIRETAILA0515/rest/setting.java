794ea7ad748fee422333bd87cfb400ed6f188f30
Nathanaja12 <86678851+Nathanaja12@users.noreply.github.com>
2021-12-24 13:43:53 +0700

    Update setting.java

--- src/main/java/APAP/SIRETAILA0515/rest/setting.java
+++ src/main/java/APAP/SIRETAILA0515/rest/setting.java
@@ -2,6 +2,7 @@ package APAP.SIRETAILA0515.rest;
 
 public class setting {
     final public static String Item = "https://si-item.herokuapp.com";
-    final public static String Coupon = "https://c93627e8-811a-4441-9f4b-4e8222815de4.mock.pstmn.io";
+//     final public static String Coupon = "https://sibusinessa05.herokuapp.com/api/coupon_list";
+   final public static String Coupon = "https://c93627e8-811a-4441-9f4b-4e8222815de4.mock.pstmn.io";
     final public static String itemRequest = "https://6486492c-2c38-4046-a782-d403388ab6e3.mock.pstmn.io/";
 }
